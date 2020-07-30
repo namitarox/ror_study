4fc8772ec60e373fd0714fcf492bebf5be50c7d8
浪岡大貴 <namioka@pixel-hearts.co.jp>
2020-07-30 13:58:13 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,42 @@
 Rails.application.routes.draw do
+  resources :answers
+  resources :questions
+  resources :mycontacts
+  get 'messages/index'
+  get 'messages', to: 'messages#index'
+
+  get 'messages/add'
+  post 'messages/add', to: 'messages#create'
+
+  get 'messages/edit/:id', to: 'messages#edit'
+  patch 'messages/edit/:id', to: 'messages#update'
+
+  get 'messages/delete/:id', to: 'messages#delete'
+  get 'messages/:id', to: 'messages#show'
+
+  get 'cards/index'
+  get 'cards', to: 'cards#index'
+
+  get 'cards/add'
+  post 'cards/add'
+
+  get 'cards/:id', to: 'cards#show'
+
+  get 'cards/edit/:id', to: 'cards#edit'
+  patch 'cards/edit/:id', to: 'cards#edit'
+
+  get 'cards/delete/:id', to: 'cards#delete'
+
+  get 'people/index'
+  get 'people', to: 'people#index'
+  get 'people/add'
+  post 'people/add', to: 'people#create'
+  get 'people/find'
+  post 'people/find'
+  get 'people/:id', to: 'people#show'
+  get 'people/edit/:id', to: 'people#edit'
+  post 'people/edit/:id', to: 'people#update'
+  patch 'people/edit/:id', to: 'people#update'
+  get 'people/delete/:id', to: 'people#delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/schema.rb
@@ -0,0 +1,67 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_28_183851) do
+
+  create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "question_id"
+    t.text "content"
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "title"
+    t.text "author"
+    t.integer "price"
+    t.text "publisher"
+    t.text "memo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "person_id"
+    t.text "title"
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "mycontacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "name"
+    t.integer "age"
+    t.boolean "nationality"
+    t.text "mail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "name"
+    t.integer "age"
+    t.text "mail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.text "name"
+    t.boolean "finished"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
